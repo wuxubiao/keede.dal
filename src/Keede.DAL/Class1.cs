36215e1fa7d56ac274ae9339153d267860dd5a50
xboo <bobit@live.cn>
2017-06-27 09:19:47 +0800

    【other】 initial  project

---
+++ src/Keede.DAL/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using Dapper;
+
+namespace Keede.DAL
+{
+    class Sql
+    {
+
+        public void QueryFirst()
+        {
+            var conn= Database.GetDbConnection();
+            conn.QueryFirst
+        }
+    }
+}
---
+++ src/Keede.DAL/Database.cs
@@ -0,0 +1,20 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+using Config.Keede.Library;
+
+namespace Keede.DAL
+{
+    public class Database
+    {
+        static Database()
+        {
+            var connectionString = ConfManager.GetAppsetting("");
+        }
+
+        public static IDbConnection GetDbConnection()
+        {
+            return new SqlConnection();
+        }
+    }
+}
