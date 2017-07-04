fc116ae827b08e838c741b4d2a4664dfa4c46e86
吴旭标 <wu_xubiao@163.com>
2017-07-04 14:25:37 +0800

    [重构]Attribute从SqlMapperExtensions移出来，类名文件一一对应

---
+++ src/Dapper/Extension/Attribute/AutoKeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper.Extension
+{
+    /// <summary>
+    /// 自动主键KEY，在Insert是会被过滤掉，适合用于自增主键和数据库自动赋值主键
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class AutoKeyAttribute : Attribute
+    {
+    }
+}
---
+++ src/Dapper/Extension/Attribute/ExplicitKeyAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dapper.Extension
+{
+    /// <summary>
+    /// 明确主键，可用于Insert时添加进去
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ExplicitKeyAttribute : Attribute
+    {
+    }
+}
